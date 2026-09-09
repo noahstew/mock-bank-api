9fa480f47786890e0b9b050dfd0d7d3830fa5e90
Noah Stewart <njstewart22@icloud.com>
2026-09-09 15:37:58 -0700

    feature(api): Adding account api routes for create, listing and deleting accounts.

---
+++ api/server.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/noahstew/bank-api/db/sqlc"
+)
+
+// serves http requests
+type Server struct {
+	store *db.Store
+	router *gin.Engine
+}
+
+// creates new http server and sets up routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/account/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+
+	server.router = router 
+
+	return server
+}
+
+// runs http server on an address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
