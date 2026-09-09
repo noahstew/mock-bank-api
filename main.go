72db44d8a4e60241557ad5fa55a3a5538efef1d9
Noah Stewart <njstewart22@icloud.com>
2026-09-09 16:47:14 -0700

    feature: Adding .env to the project via viper.

--- main.go
+++ main.go
@@ -7,17 +7,17 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/noahstew/bank-api/api"
 	db "github.com/noahstew/bank-api/db/sqlc"
+	"github.com/noahstew/bank-api/util"
 )
 
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/bank-api?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
@@ -26,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server")
 	}
