a16889951b1ac84c1ca2dbcc939349419de7683d
Noah Stewart <njstewart22@icloud.com>
2026-09-12 10:32:47 -0700

    feature(db): Updating user tests to hash passwords

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -10,9 +10,12 @@ import (
 )
 
 func createRandomUser(t *testing.T) User {
+	hashPw, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
 	arg := CreateUserParams{
 		Username: util.RandomOwner(),
-		HashedPassword: "secret",
+		HashedPassword: hashPw,
 		FullName: util.RandomOwner(),
 		Email: util.RandomEmail(),
 	}
